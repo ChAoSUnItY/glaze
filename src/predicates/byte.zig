60d043173b11f5a18289615761772afb2fefc621
Kyle Lin <minecraft.kyle.train@gmail.com>
2023-04-30 15:30:39 +0800

    add more byte functions

--- src/predicates/byte.zig
+++ src/predicates/byte.zig
@@ -10,3 +10,25 @@ pub fn isAlpha(char: u8) bool {
         },
     }
 }
+
+pub fn isDigit(char: u8) bool {
+    switch (char) {
+        '0'...'9' => {
+            return true;
+        },
+        else => {
+            return false;
+        },
+    }
+}
+
+pub fn isHex(char: u8) bool {
+    switch (char) {
+        '0'...'9', 'A'...'F', 'a'...'f' => {
+            return true;
+        },
+        else => {
+            return false;
+        },
+    }
+}
