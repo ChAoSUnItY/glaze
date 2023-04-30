7c4420210ce505dec484e2bbe68c66b9eab90cfe
Kyle Lin <minecraft.kyle.train@gmail.com>
2023-05-01 02:39:13 +0800

    Implement a hacky parser interface to avoid pointer casting in user impl aspect

--- src/main.zig
+++ src/main.zig
@@ -1,7 +1,17 @@
 const std = @import("std");
+const parsers = @import("parsers.zig");
 const bytes_parser = @import("parsers/byte.zig");
 const bytes_predicate = @import("predicates/byte.zig");
 const testing = std.testing;
+
+fn parse(input: []const u8, context: []const u8) parsers.ExperimentParser([]const u8, u8, []const u8).ParserResult {
+    _ = context;
+
+    return .{
+        .output = input[0],
+        .remain = input[1..],
+    };
+}
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
@@ -10,8 +20,12 @@ pub fn main() !void {
 
     const allocator = gpa.allocator();
 
-    const result = try bytes_parser.alpha().invoke("ABC");
-    std.debug.print("{}\n", .{result});
-    const tagResult = try (try bytes_parser.satisfy(allocator, bytes_predicate.isAlpha)).invoke("1ACD");
-    std.debug.print("{}\n", .{tagResult});
+    // const result = try bytes_parser.alpha().invoke("ABC");
+    // std.debug.print("{}\n", .{result});
+    // const tagResult = try (try bytes_parser.satisfy(allocator, bytes_predicate.isAlpha)).invoke("1ACD");
+    // std.debug.print("{}\n", .{tagResult});
+
+    const experimentParser = try parsers.ExperimentParser([]const u8, u8, []const u8).initStringLiteralCtx(allocator, "AKA", parse);
+
+    std.debug.print("{}\n", .{try experimentParser.invoke("AKA")});
 }
--- src/parsers.zig
+++ src/parsers.zig
@@ -2,9 +2,63 @@ const std = @import("std");
 const Allocator = std.mem.Allocator;
 
 pub const ParserError = error{
+    Unimplemented,
     IncompleteData,
     InvalidData,
 };
+
+pub fn ParserContext(comptime T: type) type {
+    return struct {
+        data: T,
+    };
+}
+
+pub fn ExperimentParser(comptime I: type, comptime O: type, comptime R: type) type {
+    return struct {
+        const Self = @This();
+        pub const ResultTuple = struct { output: O, remain: R };
+        pub const ParserResult = ParserError!ResultTuple;
+        pub const ParserFunction = *const fn (I, ?[]align(16) u8) ParserResult;
+
+        delegatedData: ?[]align(16) u8,
+        allocator: ?Allocator,
+        parser: ParserFunction,
+
+        pub fn initStringLiteralCtx(allocator: Allocator, comptime data: []const u8, comptime parser: *const fn (I, []const u8) ParserResult) !Self {
+            return Self.init(allocator, data, parser);
+        }
+
+        pub fn init(allocator: Allocator, comptime data: anytype, comptime parser: *const fn (I, @TypeOf(data)) ParserResult) !Self {
+            const T = @TypeOf(data);
+            const delegatedData = try allocator.alignedAlloc(u8, 16, @sizeOf(T));
+            errdefer allocator.free(delegatedData);
+            @ptrCast(*T, delegatedData.ptr).* = data;
+
+            const parserImpl = struct {
+                fn parse(input: I, context: ?[]align(16) u8) ParserResult {
+                    return @call(.{}, parser, .{ input, @ptrCast(*@TypeOf(data), context.?.ptr).* });
+                }
+            };
+
+            return .{
+                .delegatedData = delegatedData,
+                .allocator = allocator,
+                .parser = parserImpl.parse,
+            };
+        }
+
+        pub fn invoke(self: *const Self, input: I) ParserResult {
+            defer self.deinit();
+            return try self.parser(input, self.delegatedData);
+        }
+
+        pub fn deinit(self: Self) void {
+            if (self.delegatedData) |delegatedData| {
+                self.allocator.?.free(delegatedData);
+            }
+        }
+    };
+}
 
 pub fn Parser(comptime I: type, comptime O: type, comptime R: type) type {
     return struct {
