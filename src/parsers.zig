b0c1a3b41986e1c95e7062e7e18b0b6afdea30dd
Kyle Lin <minecraft.kyle.train@gmail.com>
2023-05-02 03:22:31 +0800

    add more initializers for Parser

--- src/parsers.zig
+++ src/parsers.zig
@@ -14,28 +14,46 @@ pub fn ExperimentParser(comptime I: type, comptime O: type, comptime R: type) ty
         pub const ParserResult = ParserError!ResultTuple;
         pub const ParserFunction = *const fn (I, ?[]align(16) u8) ParserResult;
 
-        delegatedData: ?[]align(16) u8,
+        parserCtx: ?[]align(16) u8,
         allocator: ?Allocator,
         parser: ParserFunction,
 
-        pub fn initStringLiteralCtx(allocator: Allocator, comptime data: []const u8, comptime parser: *const fn (I, []const u8) ParserResult) !Self {
-            return Self.init(allocator, data, parser);
+        /// Inits `Parser` while having parser function accpets unsized string literal, this is useful when parameter `parser` requires unsized string
+        pub fn initStringLiteralCtx(allocator: Allocator, comptime parserCtx: []const u8, comptime parser: *const fn (I, []const u8) ParserResult) Allocator.Error!Self {
+            return Self.init(allocator, parserCtx, parser);
         }
 
-        pub fn init(allocator: Allocator, comptime data: anytype, comptime parser: *const fn (I, @TypeOf(data)) ParserResult) !Self {
-            const T = @TypeOf(data);
-            const delegatedData = try allocator.alignedAlloc(u8, 16, @sizeOf(T));
-            errdefer allocator.free(delegatedData);
-            @ptrCast(*T, delegatedData.ptr).* = data;
-
+        /// Inits `Parser` without any predefined context.
+        pub fn initImmediateCtx(parser: *const fn (I, void) ParserResult) Self {
             const parserImpl = struct {
-                fn parse(input: I, context: ?[]align(16) u8) ParserResult {
-                    return @call(.{ .modifier = .always_inline }, parser, .{ input, @ptrCast(*@TypeOf(data), context.?.ptr).* });
+                fn parse(input: I, _: ?[]align(16) u8) ParserResult {
+                    return @call(.{ .modifier = .always_inline }, parser, .{ input, void{} });
                 }
             };
 
             return .{
-                .delegatedData = delegatedData,
+                .parserCtx = null,
+                .allocator = null,
+                .parser = parserImpl.parse,
+            };
+        }
+
+        /// Inits `Parser` with parameter `parserCtx` and `parser`, parameter `parser`'s 2nd parameter type is based on parameter `parserCtx`, therefore
+        /// no direct pointer casting is required in parser function implementation.
+        pub fn init(allocator: Allocator, comptime parserCtx: anytype, comptime parser: *const fn (I, @TypeOf(parserCtx)) ParserResult) Allocator.Error!Self {
+            const T = @TypeOf(parserCtx);
+            const parserCtxSlice = try allocator.alignedAlloc(u8, 16, @sizeOf(T));
+            errdefer allocator.free(parserCtxSlice);
+            @ptrCast(*T, parserCtxSlice.ptr).* = parserCtx;
+
+            const parserImpl = struct {
+                fn parse(input: I, context: ?[]align(16) u8) ParserResult {
+                    return @call(.{ .modifier = .always_inline }, parser, .{ input, @ptrCast(*@TypeOf(parserCtx), context.?.ptr).* });
+                }
+            };
+
+            return .{
+                .parserCtx = parserCtxSlice,
                 .allocator = allocator,
                 .parser = parserImpl.parse,
             };
@@ -43,12 +61,12 @@ pub fn ExperimentParser(comptime I: type, comptime O: type, comptime R: type) ty
 
         pub fn invoke(self: *const Self, input: I) ParserResult {
             defer self.deinit();
-            return try self.parser(input, self.delegatedData);
+            return try self.parser(input, self.parserCtx);
         }
 
         pub fn deinit(self: Self) void {
-            if (self.delegatedData) |delegatedData| {
-                self.allocator.?.free(delegatedData);
+            if (self.parserCtx) |parserCtx| {
+                self.allocator.?.free(parserCtx);
             }
         }
     };
