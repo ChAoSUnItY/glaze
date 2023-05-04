359f770f2cea657cc519bf01b71b9f01b28ce996
Kyle Lin <minecraft.kyle.train@gmail.com>
2023-05-04 23:30:58 +0800

    improve tests

--- src/byte/predicates.zig
+++ src/byte/predicates.zig
@@ -34,16 +34,46 @@ pub fn isHex(char: u8) bool {
     }
 }
 
-test "Test byte predicates" {
-    var char: u8 = 'A';
+// Tests
 
-    while (char != 'Z') : (char += 1) {
+fn genRange(comptime start: u8, comptime end: u8) [end - start + 1]u8 {
+    if (end < start) {
+        @compileError("parameter end must larger than start");
+    }
+    comptime var i = start;
+    var range: [end - start + 1]u8 = undefined;
+    inline while (i <= end) : (i += 1) {
+        range[i - start] = i;
+    }
+    return range;
+}
+
+test "isAlpha" {
+    for (genRange('a', 'z')) |char| {
         try testing.expect(isAlpha(char));
     }
 
-    char = 'a';
-
-    while (char != 'z') : (char += 1) {
+    for (genRange('A', 'Z')) |char| {
         try testing.expect(isAlpha(char));
+    }
+}
+
+test "isDigit" {
+    for (genRange('0', '9')) |char| {
+        try testing.expect(isHex(char));
+    }
+}
+
+test "isHex" {
+    for (genRange('0', '9')) |char| {
+        try testing.expect(isHex(char));
+    }
+
+    for (genRange('a', 'f')) |char| {
+        try testing.expect(isHex(char));
+    }
+
+    for (genRange('A', 'F')) |char| {
+        try testing.expect(isHex(char));
     }
 }
